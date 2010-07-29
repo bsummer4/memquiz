892a19c55c010fce78eb844e2e5bd86f1d0740dc
Benjamin Summers <bsummer4@eecs.utk.edu>
2010-07-29 15:20:06 -0400

    Cleaned up the lojban cards.

--- lojban/numgen.tcl
+++ lojban/numgen.tcl
@@ -2,17 +2,25 @@
 
 proc this {x} { return $x }
 proc digit {} {
-    lindex {no pa re ci vo mu xa ze bi so} [expr {int(rand()*10)}] }
+	lindex {no pa re ci vo mu xa ze bi so} [expr {int(rand()*10)}] }
 
 proc number {{len 0}} {
-    times digit [if {$len} {this $len} {expr {int(rand()*9+1)} }]}
+	times [if {$len} {this $len} {expr {int(rand()*9+1)} }] digit}
 
-proc times {proc times} {
-    set result {}
-    for {set i 0} {$i < $times} {incr i} {
-        lappend result [eval $proc] }
-    return $result }
+proc withresult script {
+	uplevel {set result {}}
+	upvar result r
+	uplevel $script
+	return $r }
 
+proc dotimes {n script} { for {} {$n > 0} {incr n -1} { uplevel $script }}
+proc times {n script} {
+	withresult {
+		dotimes $n { lappend result [uplevel $script] }}}
+
+set len 2
 while {![eof stdin]} {
-    gets stdin
-    times {puts [number 2]} 10 }
+	gets stdin l;
+	set l [string trim $l]
+	if {[string length $l] && [string is integer $l]} { set len $l }
+	dotimes 10 {puts [number $len]}}
